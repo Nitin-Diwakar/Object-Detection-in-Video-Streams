f011d12ff43b198de3b2d8090cf416803d800eb6
Nitin Diwakar <diwakarnitin509@gmail.com>
2024-02-18 14:30:32 +0530

    done

--- v1.py
+++ v1.py
@@ -11,7 +11,7 @@ st.markdown(
     """
     <style>
     .stApp {
-        background-image: linear-gradient(to top, #262626, #737373);
+        background-image: linear-gradient(to top, #ffffff, #737373);
         color: #2B2B2B;
     }
     
