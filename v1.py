52539c8ea79458b9124c0814469dc2d65709e27e
Nitin Diwakar <diwakarnitin509@gmail.com>
2024-02-18 15:18:59 +0530

    jf

--- v1.py
+++ v1.py
@@ -25,7 +25,7 @@ st.markdown(
         border-color: #0056b3;
     }
     h1, h2, h3, h4, h5, h6 {
-        color: #fa3e3e;
+        color: #007BFF;
     }
     .css-145kmo2 {
         background-color: #333940;
